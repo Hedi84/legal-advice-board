2148b2988def6fbc20f6f7d3e51128541d092549
Hedi <37661027+Hedi84@users.noreply.github.com>
2026-06-29 01:15:22 +0100

    Skip asynchronous form submission when payment delay is disabled

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -5,12 +5,12 @@ export default class extends Controller {
   static values = { delay: { type: Number, default: 3000 } }
 
   pay(event) {
+    if (this.delayValue === 0) return
+
     event.preventDefault()
     this.buttonTarget.textContent = "Processing..."
     this.buttonTarget.disabled = true
 
-    setTimeout(() => {
-      this.element.requestSubmit()
-    }, this.delayValue)
+    setTimeout(() => { this.element.requestSubmit() }, this.delayValue)
   }
 }
